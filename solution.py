b40bb87ca89fbfb4d71e1f0717926e4fdc83b7c5
saadalhuthali <96408381+saadalhuthali@users.noreply.github.com>
2024-07-30 22:41:13 +0300

    Solution.py

---
+++ solution.py
@@ -0,0 +1,62 @@
+import requests
+
+
+def perform_get_request():
+    """Perform GET request to given URL and return the response"""
+    url = 'https://httpbin.org/get'
+    response = requests.get(url)
+    return response
+
+
+def perform_get_request_with_params():
+    """Perform GET request to given URL sending any parameter and return the response"""
+    url = 'https://httpbin.org/get'
+    response = requests.get(url, params={'user_id': '123'})
+    return response
+
+
+def perform_post_request():
+    """Perform POST request to given URL sending given data and return the response"""
+    url = 'https://httpbin.org/post'
+    data = {
+        'first_name': 'Guido',
+        'last_name': 'van Rossum'
+    }
+    response = requests.post(url, json=data)
+    return response
+
+
+def perform_put_request():
+    """Perform PUT request to given URL sending given data and return the response"""
+    url = 'https://httpbin.org/put'
+    data = {
+        'first_name': 'Guido',
+        'last_name': 'van Rossum'
+    }
+    response = requests.put(url, json=data)
+    return response
+
+
+def perform_patch_request():
+    """Perform PATCH request to given URL sending given data and return the response"""
+    url = 'https://httpbin.org/patch'
+    data = {
+        'first_name': 'Guido'
+    }
+    response = requests.patch(url, json=data)
+    return response
+
+
+def perform_delete_request():
+    """Perform DELETE request to given URL and return the response"""
+    url = 'https://httpbin.org/delete'
+    response = requests.delete(url)
+    return response
+
+
+def perform_redirect_request():
+    """Perform a request to a redirect URL and return the Location header that comes in the response"""
+    # HINT: you should use the allow_redirects parameter while doing the request
+    url = 'https://httpbin.org/redirect/1'
+    response = requests.get(url, allow_redirects=False)
+    return response.headers['Location']
